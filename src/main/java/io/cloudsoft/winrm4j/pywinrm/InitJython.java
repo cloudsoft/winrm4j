8caac49399869c3b4ff3d0fc3679446a6dc217ff
Martin Harris <github@nakomis.com>
2015-04-01 16:13:19 +0100

    Updates sample InitJython to download and install 7-Zip

--- src/main/java/io/cloudsoft/winrm4j/pywinrm/InitJython.java
+++ src/main/java/io/cloudsoft/winrm4j/pywinrm/InitJython.java
@@ -8,7 +8,12 @@ public class InitJython {
         SessionType session = WinRMFactory.INSTANCE.createSession(args[0],
                 args[1], args[2]);
 
-        ResponseType response = session.run_cmd("ipconfig", "/all");
+        ResponseType response = session.run_ps("(new-object System.Net.WebClient).DownloadFile(\"http://www.7-zip.org/a/7z938-x64.msi\", \"C:\\setup.msi\")");
+        System.out.println(response.getStdOut());
+        System.out.println(response.getStdErr());
+        System.out.println(response.getStatusCode());
+
+        response = session.run_ps("Start-Process msiexec.exe -ArgumentList \"/i c:\\setup.msi /qn\" -Wait");
         System.out.println(response.getStdOut());
         System.out.println(response.getStdErr());
         System.out.println(response.getStatusCode());
