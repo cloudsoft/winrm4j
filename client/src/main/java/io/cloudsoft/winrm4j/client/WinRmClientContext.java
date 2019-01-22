0ef319da74f73e99597da8b803578b1219b071d7
Aled Sage <aled.sage@gmail.com>
2019-01-22 17:38:17 +0000

    Issue-81: bus configurable for WinRmClientContext

--- client/src/main/java/io/cloudsoft/winrm4j/client/WinRmClientContext.java
+++ client/src/main/java/io/cloudsoft/winrm4j/client/WinRmClientContext.java
@@ -1,5 +1,7 @@
 package io.cloudsoft.winrm4j.client;
 
+import static java.util.Objects.requireNonNull;
+
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.transport.http.asyncclient.AsyncHTTPConduit;
@@ -9,9 +11,27 @@ import org.apache.cxf.transport.http.asyncclient.AsyncHTTPConduitFactory.UseAsyn
 public class WinRmClientContext {
     public static WinRmClientContext newInstance() {
         Bus bus = configureBus(BusFactory.newInstance().createBus());
-        return new WinRmClientContext(bus);
+        return new WinRmClientContext(bus, true);
     }
 
+    /**
+     * Uses the given {@code bus}.
+     * 
+     * The bus should be pre-configured with:
+     * <pre>
+     * bus.getProperties().put(AsyncHTTPConduit.USE_ASYNC, Boolean.TRUE);
+     * bus.getProperties().put(AsyncHTTPConduitFactory.USE_POLICY, UseAsyncPolicy.ALWAYS);
+     * </pre>
+     * This needs to be async to force the use of Apache HTTP Components client.
+     * Details at http://cxf.apache.org/docs/asynchronous-client-http-transport.html.
+     * 
+     * Note the {@link #shutdown()} method will not shutdown the given bus - that is
+     * the responsibility of the caller who supplied the bus. 
+     */
+    public static WinRmClientContext newInstance(Bus bus) {
+        return new WinRmClientContext(bus, false);
+    }
+    
     static Bus configureBus(Bus bus) {
         // Needed to be async to force the use of Apache HTTP Components client.
         // Details at http://cxf.apache.org/docs/asynchronous-client-http-transport.html.
@@ -22,13 +42,17 @@ public class WinRmClientContext {
     }
 
     private final Bus bus;
+    private final boolean cleanupBus;
 
-    private WinRmClientContext(Bus bus) {
-        this.bus = bus;
+    private WinRmClientContext(Bus bus, boolean cleanupBus) {
+        this.bus = requireNonNull(bus, "bus");
+        this.cleanupBus = cleanupBus;
     }
 
     public void shutdown() {
-        bus.shutdown(true);
+    	if (cleanupBus) {
+    		bus.shutdown(true);
+    	}
     }
 
     Bus getBus() {
